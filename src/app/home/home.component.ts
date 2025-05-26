a05f8c49cb46f622f7e2ff49a94f2a5c9684e501
Lore <khasanboykupalov1@gmail.com>
2025-05-27 01:58:11 +0500

    start shon in angular

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ShopComponent } from "../shop/shop.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ShopComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
